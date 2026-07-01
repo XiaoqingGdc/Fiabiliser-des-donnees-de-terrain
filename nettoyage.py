50a1d0f6b1433592d60a51a8b3b7d9708e750db3
Xiaoqing <xiaoqing.zhou@outlook.fr>
2026-07-01 21:16:16 +0200

    correction:filtrage des nagtifs

--- nettoyage.py
+++ nettoyage.py
@@ -1,19 +1,22 @@
 import pandas as pd
+import numpy as np
 df=pd.read_csv('production_brute.csv', sep=';')
 print(df.head(8))
 print(df.columns)
-df["site"] = df["site"].str.strip().str.title()
-df["horodatage"]=pd.to_datetime(df["horodatage"])
+df["site"] = df["site"].str.strip().str.lower()
+df["horodatage"]=pd.to_datetime(df["horodatage"], format='%Y-%m-%d %H:%M', errors='coerce')
 
 df["production"]=pd.to_numeric(df["production"].str.replace(",", "."), errors="coerce")
-condition = df['production'] < 0
-df.loc[condition, 'production'] = 0
 
-condition = (df['unite'])=="MW"
+condition  = (df['production'] < 0)
+df.loc[condition, 'production'] = np.nan
+
+condition = (df['unite']=="MW")
 df.loc[condition, 'production'] = df.loc[condition, 'production'] * 1000
 
 df['unite'] = 'kW'
-df=df.drop_duplicates()
 
+df=df.drop_duplicates()
+print('------------file cleaned------------')
 print(df.head(8))
 print(df.shape)
